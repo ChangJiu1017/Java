26ee088f2bfc70395480030b680a54abe62bc76f
ChangJiu1017 <changxinyu001@gmail.com>
2024-02-28 21:16:47 +0800

    修改函数调用

--- src/Copilot/Copilot.java
+++ src/Copilot/Copilot.java
@@ -5,4 +5,16 @@ public class Copilot {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    // This is a new method
+    public void newMethod() {
+        System.out.println("This is a new method");
+    }
+
+    // 写一个可以返回两个数相加的方法
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
 }
